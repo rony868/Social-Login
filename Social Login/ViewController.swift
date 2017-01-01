7cca2d3056c6277b15cfd179b3440b242aa86343
Md.Rasheduzzaman <md.rasheduzzaman@MdRasheduzzamans-Mac.local>
2017-01-01 07:08:51 -0800

    Initial Commit

---
+++ Social Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Login
+//
+//  Created by Md.Rasheduzzaman on 1/1/17.
+//  Copyright © 2017 codertuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
